4c8a6d8bcb3722aa0a00da58749846e2aad7324e
kfriedman <kfriedman@H10897.ad.hugeinc.com>
2021-01-19 09:25:27 -0500

    updated analytics file

--- src/app.js
+++ src/app.js
@@ -4,12 +4,12 @@ import AppRouter from './routes/AppRoute';
 import Modal from 'react-modal';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
-//import wireUpAnalytics from './analytics/analytics-bootstrap';
+import wireUpAnalytics from './analytics/analytics-bootstrap';
 
 
 Modal.setAppElement('#root');
 ReactDOM.render(<AppRouter />, document.getElementById('root'));
 
 // wire up analytics 
-//wireUpAnalytics();
+wireUpAnalytics();
 
